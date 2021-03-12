8fe0d375a96252c04bd20c06ee9dc375f7c5f097
Vivek-Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2021-03-12 17:58:06 +0530

    database model

--- website/models.py
+++ website/models.py
@@ -1,1 +1,10 @@
-import flask
+from . import db
+from sqlalchemy.sql import func
+
+class URL_DB(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    long_url = db.Column(db.String(300))
+    short_url = db.Column(db.String(50))
+    ip_address = db.Column(db.String(50))
+    visits = db.Column(db.Integer)
+    date = db.Column(db.DateTime(timezone = True), default = func.now())
